e9e1577226e8a3da0a6ccf13543fcad4f145c3a5
Basar Buyukkahraman <basarbk@yahoo.com>
2021-02-28 12:01:45 +0300

    Use @Value with default value

--- src/main/java/com/example/email/configuration/MailConfiguration.java
+++ src/main/java/com/example/email/configuration/MailConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Configuration
 public class MailConfiguration {
 
-  @Value("${mail.host}")
+  @Value("${mail.host:smtp.ethereal.email}")
   String mailHost;
 
   @Bean

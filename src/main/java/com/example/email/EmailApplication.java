653f4c9e8ccf0e43d87d0669c8a7b30020e3e33a
Basar Buyukkahraman <basarbk@yahoo.com>
2021-03-07 12:38:14 +0300

    Add production properties

--- src/main/java/com/example/email/EmailApplication.java
+++ src/main/java/com/example/email/EmailApplication.java
@@ -1,13 +1,23 @@
 package com.example.email;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmailApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(@Value("${message}") String message){
+		return (args) -> {
+			System.err.println(message);
+		};
 	}
 
 }
